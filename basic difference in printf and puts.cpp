89c7d869a948446ce61daf10201cf6d568fcc514
Jayantnis <65399679+Jayantnis@users.noreply.github.com>
2020-05-15 16:56:34 +0530

    Add files via upload

---
+++ basic difference in printf and puts.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{	//basic difference in C program  on printf() and puts() so
+	printf("\n=====Difference between printf() and puts()======\n");
+	printf("\n======printf==========\n");
+	
+	printf("hello");
+	printf("there");
+	
+	printf("\n=========puts()=======\n");
+	
+	puts("hello");
+	puts("there");
+return 0;
+}
